41761727e00da0ef21217e84c89d2a83e5d0e6cd
MOUNIM Yassmine <ya.mounim@esisa.ac.ma>
2026-05-16 12:54:19 +0000

    update2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+
 const nom=document.getElementById("name");
 const email=document.getElementById("email");
 const password=document.getElementById("password");
 
 const button=document.getElementById("signup");
+
 button.addEventListener("click",function(){
     if(nom.value=="" || email.value=="" || password.value==""){
-        alert("Veuillez remplir tous les champs");
-    }   else{
-        alert("Inscription réussie");
-    }       
+   const name = nom.value;
+   const mail = email.value;
+   const pass = password.value;
+   if(name=="" || mail=="" || pass==""){
+     return alert("Please fill in all the fields.");
+   }
+    }
+    const users=JSON.parse(localStorage.getItem("users")) || [];
+    users.push({nom: name, email: mail, password: pass});
+    localStorage.setItem("users", JSON.stringify(users));
+    alert(`Signup successful! Name: ${name}, Email: ${mail}, Password: ${pass}`);
+    nom.value=email.value=password.value="";
 });
