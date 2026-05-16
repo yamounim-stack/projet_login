291ce9d1b0c29e99737e2dbaa406d94ed10fb3f6
MOUNIM Yassmine <ya.mounim@esisa.ac.ma>
2026-05-16 13:06:04 +0000

    update3

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-
-const nom=document.getElementById("name"); 
+const nom=document.getElementById("nom");
 const email=document.getElementById("email");
 const password=document.getElementById("password");
+const btn=document.getElementById("btn");
 
-const button=document.getElementById("signup");
+btn.addEventListener("click",()=>{  
+    const nomVal=nom.value;
+    const mailVal=email.value;
+    const passVal=password.value;
 
-button.addEventListener("click",function(){
-    {
-   const name = nom.value;
-   const mail = email.value;
-   const pass = password.value;
-   if( !name || !mail || !pass ){
-     return alert("Please fill in all the fields.");
-   }
-    }
-    const users=JSON.parse(localStorage.getItem("users")) || [];
-    users.push({nom: name, email: mail, password: pass});
-    localStorage.setItem("users", JSON.stringify(users));
-    alert(`Signup successful! Name: ${name}, Email: ${mail}, Password: ${pass}`);
-    nom.value=email.value=password.value="";
+if( !nomVal || !mailVal|| !passVal){
+    return alert("Veuillez remplir tous les champs");
+   } 
+
+const users=JSON.parse(localStorage.getItem("users")) || [];
+users.push({nom: nomVal, mail: mailVal, pass: passVal});
+localStorage.setItem("users",JSON.stringify(users));
+alert("Inscription réussie !nom:"+nomVal+"email:"+mailVal);
 });
