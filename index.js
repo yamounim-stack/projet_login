034196cf2416f5c2485eca79957146d5fee4a2b3
MOUNIM Yassmine <ya.mounim@esisa.ac.ma>
2026-05-16 13:36:14 +0000

    update7

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const nom = document.getElementById("nom");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const btn = document.getElementById("btn");
-document.getElementById("logout-btn").addEventListener("click", () => {
-    showPage('page-login');
-});
 
 btn.addEventListener("click", () => {
     const nomVal = nom.value;
